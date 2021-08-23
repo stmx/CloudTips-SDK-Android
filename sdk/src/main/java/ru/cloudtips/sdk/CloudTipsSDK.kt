cf9be81af3b938ccf86ea290b6ca226d28d05c0a
Максим Стулин <razrab.76.pf@gmail.com>
2021-08-23 11:08:44 +0300

    Added activity result API

--- sdk/src/main/java/ru/cloudtips/sdk/CloudTipsSDK.kt
+++ sdk/src/main/java/ru/cloudtips/sdk/CloudTipsSDK.kt
@@ -1,30 +1,60 @@
 package ru.cloudtips.sdk
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.activity.result.contract.ActivityResultContract
 import androidx.appcompat.app.AppCompatActivity
 import ru.cloudtips.sdk.ui.TipsActivity
 
 interface CloudTipsSDK {
 
-	fun start(configuration: TipsConfiguration, from: AppCompatActivity, requestCode: Int)
+    fun start(configuration: TipsConfiguration, from: AppCompatActivity, requestCode: Int)
 
-	enum class TransactionStatus {
-		Succeeded,
-		Cancelled;
-	}
-	enum class IntentKeys {
-		TransactionStatus;
-	}
+    enum class TransactionStatus {
+        Succeeded,
+        Cancelled;
+    }
 
-	companion object {
-		fun getInstance(): CloudTipsSDK {
-			return CloudTipsSDKImpl()
-		}
-	}
+    enum class IntentKeys {
+        TransactionStatus;
+    }
+
+    companion object {
+        fun getInstance(): CloudTipsSDK {
+            return CloudTipsSDKImpl()
+        }
+
+        fun getContract(): ActivityResultContract<TipsConfiguration, TransactionStatus?> {
+            return CloudTipsContract()
+        }
+    }
 }
 
-internal class CloudTipsSDKImpl: CloudTipsSDK {
+internal class CloudTipsSDKImpl : CloudTipsSDK {
 
-	override fun start(configuration: TipsConfiguration, from: AppCompatActivity, requestCode: Int) {
-		from.startActivityForResult(TipsActivity.getStartIntent(from, configuration), requestCode)
-	}
+    override fun start(
+		configuration: TipsConfiguration,
+		from: AppCompatActivity,
+		requestCode: Int,
+	) {
+        from.startActivityForResult(TipsActivity.getStartIntent(from, configuration), requestCode)
+    }
+}
+
+internal class CloudTipsContract :
+    ActivityResultContract<TipsConfiguration, CloudTipsSDK.TransactionStatus?>() {
+    override fun createIntent(context: Context, configuration: TipsConfiguration): Intent {
+        return TipsActivity.getStartIntent(context, configuration)
+    }
+
+    override fun parseResult(resultCode: Int, result: Intent?): CloudTipsSDK.TransactionStatus? {
+        if (result == null) return null
+        if (resultCode != Activity.RESULT_OK) return null
+        return when (val transactionStatus =
+            result.getSerializableExtra(CloudTipsSDK.IntentKeys.TransactionStatus.name)) {
+            is CloudTipsSDK.TransactionStatus -> transactionStatus
+            else -> null
+        }
+    }
 }
