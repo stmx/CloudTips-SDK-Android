b7bc5a92a451ace8bce40b419f42a384ca65a622
Максим Стулин <razrab.76.pf@gmail.com>
2021-08-24 13:51:55 +0300

    Добавлена возможность использовать layoutId для вызова activity Добавлена возможность использовать activityResultApi

--- sdk/src/main/java/ru/cloudtips/sdk/TipsData.kt
+++ sdk/src/main/java/ru/cloudtips/sdk/TipsData.kt
@@ -4,7 +4,14 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-class TipsData(val phone: String, val name: String, val partner: String): Parcelable {
+class TipsData private constructor(
+    val phone: String,
+    val name: String,
+    val partner: String,
+    val layoutId: String,
+) : Parcelable {
 
-    constructor(phone: String, name: String) : this(phone, name, "")
+    constructor(phone: String, name: String, partner: String) : this(phone, name, partner, "")
+    constructor(phone: String, name: String) : this(phone, name, "", "")
+    constructor(layoutId: String) : this("", "", "", layoutId)
 }
